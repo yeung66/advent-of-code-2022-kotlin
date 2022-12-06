3f6404d70006056c8a3b91a61e312730fed0d849
YeungYeah <yeungyeah@qq.com>
2022-12-06 19:58:03 +0800

    finish day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input.sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun parseInput(input: String): List<Int> {
+        return input.split("\r\n\r\n").map { part -> part.lines().filter { it.isNotEmpty() }.sumOf { it.toInt() } }
+    }
 
-    val input = readInput("Day01")
+    val input = parseInput(readInput("1"))
     println(part1(input))
     println(part2(input))
 }
